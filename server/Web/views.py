50bdc00cae4e1e733c2c18fcbf34649788e523d6
lai hung wei <lai09150915@gmail.com>
2024-09-14 15:08:46 +0800

    ifx

--- server/Web/views.py
+++ server/Web/views.py
@@ -20,11 +20,13 @@ class RecordVisitView(APIView):
         # 獲取 IP 地址
         ip_address = request.META.get('REMOTE_ADDR', 'unknown')
 
-        # # 檢查是否是本地 IP 地址
-        # if ip_address == '127.0.0.1' or ip_address == '::1':
-        #     console.print(f"[bold green]IP Address:[/bold green] {ip_address} (Local IP, not recorded)")
-        #     return Response({"detail": "Local IP, not recorded"}, status=status.HTTP_200_OK)
+        # 檢查是否是本地 IP 地址
+        if ip_address == '127.0.0.1' or ip_address == '::1':
+            console.print(f"[bold green]IP Address:[/bold green] {ip_address} (Local IP, not recorded)")
+            return Response({"detail": "Local IP, not recorded"}, status=status.HTTP_200_OK)
 
         # 如果不是本地 IP，打印出 IP 地址
         console.print(f"[bold blue]IP Address:[/bold blue] {ip_address}")
+        visit_record = VisitRecord(ip_address=ip_address)
+        visit_record.save()
         return Response({"ip_address": ip_address}, status=status.HTTP_200_OK)
