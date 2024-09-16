2744db99691ef9fa2d587612b185b5fb263825b3
lai hung wei <lai09150915@gmail.com>
2024-09-16 23:16:01 +0800

    fix

--- web/app/components/Intro/Components/HomeInfo.tsx
+++ web/app/components/Intro/Components/HomeInfo.tsx
@@ -2,11 +2,7 @@
 
 import React, { useEffect, useRef } from "react";
 import { useRouter } from "next/navigation";
-import { gsap } from "gsap-trial";
-
-import { ScrambleTextPlugin } from "gsap-trial/ScrambleTextPlugin";
-
-/* The following plugin is a Club GSAP perk */
+import { gsap } from "gsap";
 
 import { motion } from "framer-motion";
 import { FaHandPeace } from "react-icons/fa";
@@ -19,12 +15,10 @@ interface HomeInfoProps {
   currentStage: number;
 }
 
-gsap.registerPlugin(ScrambleTextPlugin);
-
 const HomeInfo: React.FC<HomeInfoProps> = ({ currentStage }) => {
   const sectionRef = useRef<HTMLDivElement | null>(null);
-  const textRef = useRef<HTMLSpanElement | null>(null);
-  const helloTextRef = useRef<HTMLSpanElement | null>(null);
+  // const textRef = useRef<HTMLSpanElement | null>(null);
+  // const helloTextRef = useRef<HTMLSpanElement | null>(null);
 
   const { addNotification } = useAppContext();
   const router = useRouter();
@@ -54,20 +48,20 @@ const HomeInfo: React.FC<HomeInfoProps> = ({ currentStage }) => {
     }
   }, [currentStage]);
 
-  useEffect(() => {
-    if (textRef.current) {
-      gsap.to(textRef.current, {
-        duration: 1,
-        scrambleText: "Hi, I'm Lai Hung Wei",
-      });
-    }
-    if (helloTextRef.current) {
-      gsap.to(helloTextRef.current, {
-        duration: 2.5,
-        scrambleText: "I am a university student majoring in Computer Science",
-      });
-    }
-  }, []);
+  // useEffect(() => {
+  //   if (textRef.current) {
+  //     gsap.to(textRef.current, {
+  //       duration: 1,
+  //       scrambleText: "Hi, I'm Lai Hung Wei",
+  //     });
+  //   }
+  //   if (helloTextRef.current) {
+  //     gsap.to(helloTextRef.current, {
+  //       duration: 2.5,
+  //       scrambleText: "I am a university student majoring in Computer Science",
+  //     });
+  //   }
+  // }, []);
 
   const bgStyle =
     "bg-gradient-to-r from-blue-400 to-blue-600 rounded-lg p-5 shadow-lg text-white";
@@ -76,7 +70,7 @@ const HomeInfo: React.FC<HomeInfoProps> = ({ currentStage }) => {
     return (
       <div ref={sectionRef} className={`text-center py-4 px-8 mx-5 ${bgStyle}`}>
         <h1 className="sm:text-xl flex items-center justify-center">
-          <span ref={textRef} className="font-semibold mx-2"></span>{" "}
+          <span className="font-semibold mx-2">Hi, I'm Lai Hung We</span>
           <motion.div
             className="ml-2"
             animate={{ rotate: [-15, 15, -15] }}
@@ -86,7 +80,7 @@ const HomeInfo: React.FC<HomeInfoProps> = ({ currentStage }) => {
           </motion.div>
         </h1>
         <p className="mt-2">
-          <span ref={helloTextRef}></span>
+          <span>I am a university student majoring in Computer Science</span>
         </p>
       </div>
     );
